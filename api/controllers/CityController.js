c5e95cf8d5321458cacd5731af9c2f01ea922bc7
ChrisBurgin <chrisburgin95@gmail.com>
2017-10-09 16:48:01 -0400

    Removed API and Controler

--- api/controllers/CityController.js
+++
@@ -1,8 +0,0 @@
-/**
- * CityController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
-
-module.exports = {}
--- api/controllers/DocumentController.js
+++
@@ -1,8 +0,0 @@
-/**
- * DocumentController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
-
-module.exports = {}
